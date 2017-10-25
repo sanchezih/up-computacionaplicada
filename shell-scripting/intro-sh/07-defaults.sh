a5b5ee34c0ef682fc8ac795417f53b75300a33b8
Ignacio Sanchez <sanchezih@gmail.com>
2017-10-25 14:44:34 -0300

    agregado de nombres en script 07

--- shell-scripting/intro-sh/07-defaults.sh
+++ shell-scripting/intro-sh/07-defaults.sh
@@ -3,10 +3,17 @@
 clear # limpio la pantalla
 
 echo '$PS1: ' $PS1
+echo
 echo '$PS2: ' $PS2
+echo
 echo '$LOGNAME: ' $LOGNAME
+echo
 echo '$HOME: ' $HOME
+echo
 echo '$PATH: ' $PATH
+echo
 echo '$PWD: ' $PWD
+echo
 echo '$TERM: ' $TERM
+echo
 echo '$SHELL: ' $SHELL
