cc08956f4dfa08e12a92be51788d86fa7a0197ce
Ignacio Sanchez <sanchezih@gmail.com>
2019-11-17 00:01:37 -0300

    reorganizo el ejemplo 2 de fork

--- procesos/fork-ejemplo-02.c
+++ procesos/fork-ejemplo-02.c
@@ -1,26 +1,31 @@
 /* fork.c - Ejecución conjunta de procesos padre e hijo */
+
 #include <stdio.h>
 #include <unistd.h>
-main ()
-    {
-    printf ("Ejemplo de fork.\n");
+#include <sys/types.h>
+
+int main (void)
+{
+	pid_t pid = fork();
+
+	printf ("Ejemplo de fork.\n");
 
     printf ("Inicio del proceso padre. PID=%d\n", getpid ());
 
-
-    if (fork() == 0)
-
-
-	{	/* Proceso hijo */
-	printf ("Inicio proceso hijo. PID=%d, PPID=%d\n",
-	getpid (), getppid ());
-	sleep (1);
+    if (pid == 0)
+	{	
+		/* Proceso hijo */
+		printf ("Inicio proceso hijo. PID=%d, PPID=%d\n",
+		getpid (), getppid ());
+		sleep (1);
 	}
     else
-	{	/* Proceso padre */
-	printf ("Continuación del padre. PID=%d\n", getpid ());
-	sleep (1);
+	{	
+		/* Proceso padre */
+		printf ("Continuación del padre. PID=%d\n", getpid ());
+		sleep (1);
 	}
+
     printf ("Fin del proceso %d\n", getpid ());
-    exit (0);
-    }
+    return 0; 
+}
