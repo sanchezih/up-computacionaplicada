99834619e1f43a691be07b880c1f2aaa8a302112
Ignacio Hugo Sanchez <ihsanch@ar.ibm.com>
2020-10-25 21:12:12 -0300

    Update fork-ejemplo-02.c

--- procesos/fork-ejemplo-02.c
+++ procesos/fork-ejemplo-02.c
@@ -1,31 +1,30 @@
-/* fork.c - Ejecución conjunta de procesos padre e hijo */
+/* fork.c - Ejecucion conjunta de procesos padre e hijo */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 
-int main (void)
+int main(void)
 {
+	printf("===>- EJEMPLO DE FORK -<===\n");
+
+	printf("Inicio del proceso padre (PID=%d - PPID=%d)\n", getpid(), getppid());
 	pid_t pid = fork();
 
-	printf ("Ejemplo de fork.\n");
-
-    printf ("Inicio del proceso padre. PID=%d\n", getpid ());
-
-    if (pid == 0)
-	{	
+	if (pid == 0)
+	{
 		/* Proceso hijo */
-		printf ("Inicio proceso hijo. PID=%d, PPID=%d\n",
-		getpid (), getppid ());
-		sleep (1);
+		printf("Inicio proceso hijo (PID=%d - PPID=%d)\n",
+			   getpid(), getppid());
+		sleep(1);
 	}
-    else
-	{	
+	else
+	{
 		/* Proceso padre */
-		printf ("Continuación del padre. PID=%d\n", getpid ());
-		sleep (1);
+		printf("Continuación del padre... (PID=%d - PPID=%d)\n", getpid(), getppid());
+		sleep(1);
 	}
 
-    printf ("Fin del proceso %d\n", getpid ());
-    return 0; 
+	printf("Fin del proceso %d\n", getpid());
+	return 0;
 }
